fd7d1260814b6ea62ac9e49bcac99b25c73a0a0c
luNarinXN <196093238+luNarinXN@users.noreply.github.com>
2025-02-15 22:35:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.getElementById("contactLink").addEventListener("click", function(event) {
-  event.preventDefault();
-  document.getElementById("contactModal").style.display = "block";
-});
 
-document.getElementById("closeModal").addEventListener("click", function() {
-  document.getElementById("contactModal").style.display = "none";
-});
